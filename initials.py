34dd082b5013eab41a9dc8bc489547e08c461efb
Asam404 <2425402611@live.gctu.edu.gh>
2026-05-30 01:31:02 -0800

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Prints my initials in large block letters.
+# Assignment Number: 1
+#
+# Name: Yaw Asamoah Nyante
+# STUDENT ID: 2425402611
+# Email:
+# Grader: Augustus Buckman
+#
+# On my honor, Yaw Asamoah Nyante, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #print my initials in large block letters
 
+    print()
+    print("...YAN")
+    print()
 
+    print("...YYYY....YYYY.............AA.............NNN......NNN.....")
+    print("....YYYY..YYYY............AA..AA...........NNN......NNN.....")
+    print(".....YYY..YYY............AA....AA..........NNNN.....NNN.....")
+    print("......YY.YY.............AA......AA.........NN.NN....NNN.....")
+    print(".......YYY.............AAA......AAA........NNN..NN..NNN.....")
+    print(".......YYY.............AAAAAAAAAAAA........NNN...NN.NNN.....")
+    print(".......YYY.............AAAAAAAAAAAA........NNN....NN.NN.....")
+    print(".......YYY.............AAA......AAA........NNNN....NNNN.....")
+    print(".......YYY........**...AAA......AAA...**...NNN......NNN...**")
+    print(".......YYY........**...AAA......AAA...**...NNN......NNN...**")
+
+    print()
 
 main()
